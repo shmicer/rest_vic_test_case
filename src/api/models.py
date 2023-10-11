59593c2ff0bb053075a5a59111f320e711f55e0b
Dmitrii Shmidt <shmicer@gmail.com>
2023-10-11 11:58:39 +0300

    add question model

--- src/api/models.py
+++ src/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 
+class Question(models.Model):
+    question_id = models.IntegerField(unique=True)
+    question_text = models.TextField()
+    answer_text = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
